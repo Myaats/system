804d4cfd10d4ae25fffa4e01b46b7c801d56f75f
Mats <mats@mats.sh>
2022-11-01 11:54:38 +0100

    Enable waydroid on shun

--- devices/shun/configuration.nix
+++ devices/shun/configuration.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   # Setup device
   device = {
     uuid = "b97e7998-60bf-42f9-b314-f728de1fd7c7";
@@ -14,6 +18,19 @@
     rust = true;
     texlive = true;
   };
+
+  virtualisation.waydroid.enable = true;
+
+  environment.etc."gbinder.d/waydroid.conf".source = lib.mkOverride 50 (pkgs.writeText "waydroid.conf" ''
+    [Protocol]
+    /dev/binder = aidl3
+    /dev/vndbinder = aidl3
+    /dev/hwbinder = hidl
+    [ServiceManager]
+    /dev/binder = aidl3
+    /dev/vndbinder = aidl3
+    /dev/hwbinder = hidl
+  '');
 
   system.stateVersion = "22.11";
 }
