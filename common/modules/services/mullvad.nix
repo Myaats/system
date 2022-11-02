b37bd91f68af844047ff46a5d78d644c0dd8d240
Mats <mats@mats.sh>
2022-11-02 11:44:20 +0100

    Fix tailscale w/ mullvad

--- common/modules/services/mullvad.nix
+++ common/modules/services/mullvad.nix
@@ -89,6 +89,32 @@ in {
           else {}
         );
       };
+
+      # Fix access to tailscale https://github.com/tailscale/tailscale/issues/2880#issuecomment-1207295722
+      networking.firewall.extraCommands = ''
+        ${pkgs.nftables}/bin/nft -f - <<EOF
+        table inet mullvad-ts {
+          chain exclude-outgoing {
+            type route hook output priority 0; policy accept;
+            ip daddr 100.64.0.0/10 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+            ip daddr 10.0.0.0/16 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+            ip daddr 10.11.0.0/16 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+            ip6 daddr fd7a:115c:a1e0::/48 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+          }
+          chain allow-incoming {
+            type filter hook input priority -100; policy accept;
+            iifname "tailscale0" ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+          }
+          chain exclude-dns {
+            type filter hook output priority -10; policy accept;
+            ip daddr 100.100.100.100 udp dport 53 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+            ip daddr 100.100.100.100 tcp dport 53 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+            ip daddr 10.0.0.1 udp dport 53 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+            ip daddr 10.0.0.1 tcp dport 53 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+          }
+        }
+        EOF
+      '';
     })
   ];
 }
