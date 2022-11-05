bb7c341aeb6050e647d55bce2b986c4c3790732c
Mats <mats@mats.sh>
2022-11-05 11:20:42 +0100

    Remove boot timeout and disable systemd-boot editing

--- common/boot.nix
+++ common/boot.nix
@@ -1,4 +1,12 @@
 {...}: {
-  # Clean /tmp on boot
-  boot.cleanTmpDir = true;
+  boot = {
+    # Clean /tmp on boot
+    cleanTmpDir = true;
+    loader = {
+      # Disable bootloader timeout (just press a key to show it)
+      timeout = 0;
+      # Harden systemd-boot
+      systemd-boot.editor = false;
+    };
+  };
 }
