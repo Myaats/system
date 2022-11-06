34f57034dace2a5a5604ba3bc832984cd3fa2c2a
Mats <mats@mats.sh>
2022-11-06 01:34:40 +0100

    Add .lan root ca to certificates

--- common/profiles/base.nix
+++ common/profiles/base.nix
@@ -41,4 +41,23 @@
 
   # Disable openssh password auth
   services.openssh.passwordAuthentication = false;
+
+  # Trust .lan constrained CA
+  security.pki.certificates = [
+    ''
+      .lan
+      ====
+      -----BEGIN CERTIFICATE-----
+      MIIBiTCCAS+gAwIBAgIRAMwv6VB4SyeRzZfhXnBKPuEwCgYIKoZIzj0EAwIwETEP
+      MA0GA1UEAxMGY2EubGFuMB4XDTIyMTEwNTIyNDYwNFoXDTIyMTEwNjIyNDYwNFow
+      ETEPMA0GA1UEAxMGY2EubGFuMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEJmQq
+      CaqjHj2EYBUEePGO04ZJ3WCxwJ6Wo7f/K0e38n/b82/YURtzgP2Rin1qwl4exkjO
+      U5MSc9RNwmy66zZXuqNoMGYwDgYDVR0PAQH/BAQDAgEGMBIGA1UdEwEB/wQIMAYB
+      Af8CAQEwHQYDVR0OBBYEFDmQjbHpdGXFCOpYFgjxNqTb6M5WMCEGA1UdHgEB/wQX
+      MBWgEzAFggNsYW4wCocICgAAAP8AAAAwCgYIKoZIzj0EAwIDSAAwRQIgdOp0wYeO
+      UANKw+L7a59Sy6rE11mmS1hUFdP7Yd8R/usCIQDZ9h0nyaBaRl3Rb68NVNJ8wqEz
+      WpWqcwqa/J6uVVty+w==
+      -----END CERTIFICATE-----
+    ''
+  ];
 }
