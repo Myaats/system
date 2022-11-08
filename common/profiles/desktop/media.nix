624116cf42d137fbadfa6742c7047acb64f0ecc8
Mats <mats@mats.sh>
2022-11-08 12:46:14 +0100

    Add home-manager mpv to jellyfin-mpv-shim

--- common/profiles/desktop/media.nix
+++ common/profiles/desktop/media.nix
@@ -1,5 +1,10 @@
-{pkgs, ...}: {
-  home-manager.config = {
+{
+  lib,
+  pkgs,
+  ...
+}:
+with lib; {
+  home-manager.config = {config, ...}: {
     # Enable mpv with vaapi/gpu
     programs.mpv = {
       enable = true;
@@ -11,6 +16,12 @@
         vo = "gpu";
       };
     };
+
+    # Update jellyfin-mpv-shim settings to use home-manager mpv with scripts
+    home.activation.jellyfin-mpv-shim-conf = config.lib.dag.entryAfter ["writeBoundary"] (mergeJson "~/.config/jellyfin-mpv-shim/conf.json" {
+      mpv_ext = true;
+      mpv_ext_path = "/etc/profiles/per-user/${config.home.username}/bin/mpv";
+    });
   };
 
   # Install jellyfin-mpv-shim
