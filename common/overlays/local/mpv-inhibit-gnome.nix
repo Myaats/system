378f2beef5b42261084b9428d0a9104a2ac6e265
Mats <mats@mats.sh>
2022-12-01 15:52:09 +0100

    Update mpv inhibit gnome

--- common/overlays/local/mpv-inhibit-gnome.nix
+++ common/overlays/local/mpv-inhibit-gnome.nix
@@ -1,34 +1,28 @@
 {
   stdenv,
   fetchFromGitHub,
-  pkgs,
-  lib,
+  pkg-config,
+  dbus,
+  mpv-unwrapped
 }:
 stdenv.mkDerivation rec {
-  name = "mpv_inhibit_gnome";
-  version = "9069be0f15fa2c222d78a183d4aa1477d8a8f2cd";
+  pname = "mpv_inhibit_gnome";
+  version = "0.1.3";
 
   src = fetchFromGitHub {
     owner = "Guldoman";
-    repo = "mpv_inhibit_gnome";
-    rev = version;
-    fetchSubmodules = true;
-    sha256 = "sha256-LSGg5gAQE2JpepBqhz6D6d3NlqYaU4bjvYf1F+oLphQ=";
+    repo = pname;
+    rev = "v${version}";
+    hash = "sha256-LSGg5gAQE2JpepBqhz6D6d3NlqYaU4bjvYf1F+oLphQ=";
   };
 
-  buildInputs = with pkgs; [
-    dbus
-    mpv-unwrapped
-  ];
+  nativeBuildInputs = [ pkg-config ];
 
-  nativeBuildInputs = [
-    pkgs.pkg-config
-  ];
-
-  installPhase = ''
-    mkdir -p $out/share/mpv/scripts
-    install -m 0755 "./lib/mpv_inhibit_gnome.so" $out/share/mpv/scripts
-  '';
+  buildInputs = [ dbus mpv-unwrapped ];
 
   passthru.scriptName = "mpv_inhibit_gnome.so";
+
+  installPhase = ''
+    install -D ./lib/mpv_inhibit_gnome.so $out/share/mpv/scripts
+  '';
 }
