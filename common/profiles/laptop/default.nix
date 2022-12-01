346ed3100ce152262f8d513126d7898594745d0d
Mats <mats@mats.sh>
2022-12-02 00:19:22 +0100

    Add powertop to laptop profile

--- common/profiles/laptop/default.nix
+++ common/profiles/laptop/default.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     # Inherit desktop profile
     ../desktop
@@ -6,4 +6,8 @@
 
   # All my laptops have bluetooth I guess.
   modules.hardware.bluetooth.enable = true;
+
+  # Setup powertop
+  powerManagement.powertop.enable = true;
+  environment.systemPackages = [pkgs.powertop];
 }
