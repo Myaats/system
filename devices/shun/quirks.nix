ed51a420c1e21f5e822deaec4941a10ab9613de5
Mats <mats@mats.sh>
2022-12-12 21:40:34 +0100

    Simplify tas2563 workaround

--- devices/shun/quirks.nix
+++ devices/shun/quirks.nix
@@ -30,44 +30,6 @@ in {
     group = "wheel";
   };
 
-  # Create wrapped script to fix the tas2563
-  security.wrappers.fix-tas2563 = {
-    capabilities = "cap_sys_module+ep";
-    owner = "root";
-    group = "root";
-    source = "${pkgs.runCommandCC "fix-tas2563" {
-        nativeBuildInputs = [pkgs.makeWrapper];
-      }
-      ''
-        mkdir -p $out/bin
-        cp -p ${pkgs.writeScript "fix-tas2563" ''
-          ${pkgs.kmod}/bin/rmmod snd_soc_tas2562
-          ${pkgs.kmod}/bin/insmod /run/current-system/kernel-modules/lib/modules/${kmod_ver}/kernel/sound/soc/codecs/snd-soc-tas2562.ko.xz
-        ''} $out/bin/fix-tas2563
-        wrapProgram $out/bin/fix-tas2563
-      ''}/bin/fix-tas2563";
-  };
-
-  systemd.services.fix-yoga-speakers-suspend = {
-    wantedBy = ["suspend.target"];
-    after = ["suspend.target"];
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "/run/wrappers/bin/fix-tas2563";
-    };
-  };
-
-  services.acpid = {
-    enable = true;
-    handlers = {
-      # Fix speakers after headphone unplug
-      headphone-unplugged = {
-        action = "/run/wrappers/bin/fix-tas2563";
-        event = "jack/headphone HEADPHONE unplug";
-      };
-    };
-  };
-
   # Expose input switches to userspace
   services.udev.extraRules = ''
     KERNEL=="event[0-9]*", ENV{ID_INPUT_SWITCH}=="1", MODE:="0666"
